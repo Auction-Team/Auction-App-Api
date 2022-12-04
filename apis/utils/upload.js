dc93b8f991a64e26f663fb9802ee4b70a18c3951
Nguyen Minh Dang <minhdangb3@gmail.com>
2022-12-04 23:28:05 +0700

    [DangNM] push setting upload

--- apis/utils/upload.js
+++ apis/utils/upload.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const PATH_USER = 'user'
+const PATH_USER = 'user';
+const PATH_PRODUCT = 'product';
 
 const s3Config = new AWS.S3({
     accessKeyId: process.env.AWS_IAM_USER_KEY,
@@ -29,6 +30,18 @@ const multerS3UploadUser = multerS3({
     },
 });
 
+const multerS3UploadProduct = multerS3({
+    s3: s3Config,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+        cb(null, PATH_PRODUCT + '/' + new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    },
+});
+
 const uploadImageUser = multer({
     storage: multerS3UploadUser,
     fileFilter: fileFilter,
@@ -37,5 +50,14 @@ const uploadImageUser = multer({
     },
 });
 
+const uploadSingleImageProduct = multer({
+    storage: multerS3UploadProduct,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 5, // we are allowing only 5 MB files
+    },
+});
+
 exports.uploadImageUser = uploadImageUser;
+exports.uploadSingleImageProduct = uploadSingleImageProduct;
 exports.s3 = s3Config;
