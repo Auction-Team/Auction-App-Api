0fba0c97f576585253ac294fb5b3b8eed4e27650
Phong Hồ <phonghh@unit.com.vn>
2022-12-22 11:21:38 +0700

    [PhongHH] hot fix update profile

--- apis/validations/user_validation.js
+++ apis/validations/user_validation.js
@@ -4,6 +4,9 @@ const updateProfileSchema = {
     body: Joi.object().keys({
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
+        province: Joi.string(),
+        district: Joi.string(),
+        ward: Joi.string()
     }),
 };
 
